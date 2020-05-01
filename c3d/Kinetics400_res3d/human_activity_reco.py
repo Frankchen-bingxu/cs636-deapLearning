def083ec5ba27407449b1e6cc5a0eb19af84bc55
GPW9795 <39331237+GPW9795@users.noreply.github.com>
2020-05-01 17:45:20 -0500

    update

--- c3d/Kinetics400_res3d/human_activity_reco.py
+++ c3d/Kinetics400_res3d/human_activity_reco.py
@@ -5,36 +5,38 @@ import argparse
 import imutils
 import sys
 import cv2
-import json
 
+import os
+import json
+current_path = os.path.dirname(__file__)
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 # ap.add_argument("-m", "--model", required=True,
 # 	help="path to trained human activity recognition model")
 # ap.add_argument("-c", "--classes", required=True,
 # 	help="path to class labels file")
-ap.add_argument("-i", "--video_name", type=str, default="",
-    help="optional path to video file")
+# ap.add_argument("-i", "--video_name", type=str, default="",
+#     help="optional path to video file")
 #args = vars(ap.parse_args(args=['--model','/Users/chenbingxu/PycharmProjects/c3d/Kinetics400_res3d/resnet-34_kinetics.onnx','--classes','/Users/chenbingxu/PycharmProjects/c3d/Kinetics400_res3d/action_recognition_kinetics.txt']))
 #,'--input','/Users/chenbingxu/PycharmProjects/c3d/video_test/8 types of actions.mp4'
 args = vars(ap.parse_args())
 # load the contents of the class labels file, then define the sample
 # duration (i.e., # of frames for classification) and sample size
 # (i.e., the spatial dimensions of the frame)
-CLASSES = open('/Users/chenbingxu/PycharmProjects/c3d/Kinetics400_res3d/action_recognition_kinetics.txt').read().strip().split("\n")
+CLASSES = open(current_path + '/action_recognition_kinetics.txt').read().strip().split("\n")
 SAMPLE_DURATION = 16
 SAMPLE_SIZE = 112
 
 # load the human activity recognition model
 print("[INFO] loading human activity recognition model...")
-net = cv2.dnn.readNet('/Users/chenbingxu/PycharmProjects/c3d/Kinetics400_res3d/resnet-34_kinetics.onnx')
+net = cv2.dnn.readNet(current_path + '/resnet-34_kinetics.onnx')
 
 # grab a pointer to the input video stream
 print("[INFO] accessing video stream...")
-vs = cv2.VideoCapture(args["video_name"] if args["video_name"] else 0)
+vs = cv2.VideoCapture((current_path + "/8_actions.mp4") if (current_path + "video_test/8_actions.mp4") else 0)
 
 # loop until we explicitly break from it
-submit = '/Users/chenbingxu/PycharmProjects/c3d/timeLabel.json'
+submit = 'c3d/timeLabel.json'
 result = []
 
 while True:
